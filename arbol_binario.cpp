a718d8e564785c4feb901aaf000ae7feed380227
guillermopis <guillermo.pisqui@gmail.com>
2017-04-26 00:28:54 -0600

    version3, funcion eliminar terminada

--- arbol_binario.cpp
+++ arbol_binario.cpp
@@ -4,8 +4,9 @@
 #include <stdlib.h>
 #include <windows.h>
 using namespace std;
+int contador=0;
+int cont=0;
 int bandera=0;
-
 class nodo {
 	//dESDE OTRA CLASE SE PUEDEN acceder a estos atributos
    public:
@@ -34,9 +35,10 @@ nodo *crearnodo(int,nodo *);
 
 
 nodo *arbol = NULL; //incializa el arbol en NULL
-nodo *arbol2 = NULL; //incializa el arbol en NULL
-nodo *arbol3 = NULL;
-
+nodo *anterior_eliminar = NULL; //incializa el arbol en NULL
+nodo *raiz=NULL;//guarda un apuntador a la raiz del arbol;
+nodo *subderecho=NULL;
+nodo *subizquierdo=NULL;
 typedef nodo *pnodo;
 
 class lista {
@@ -44,16 +46,21 @@ class lista {
    	void insertarnodo(nodo *&, int, nodo * );
    	void mostrararbol(nodo *); //funcion para mostrar el arbol con recursividad
    	void mostrararbol2(nodo *); //FUNCIOIN PARA RECCORER EL arbol con while
-   	void buscar(int , nodo *);
+   	int buscar(int , nodo *);
+   	int eliminar(int, nodo *);
+   	int aniadir(nodo *);
+   	int identificar(int,nodo *);
+   	int caracteristicas(int, nodo*);
+   	
    private:
     pnodo primero;
     pnodo actual;
 };
 
-void mostar(){
-	
-}
-bool encontrado=false;
+
+
+
+bool encontrado;
 int main() {
 	menu();
    system("pause");
@@ -66,10 +73,13 @@ void menu(){
 		system("cls");
 		cout<<"--------------ARBOLES---------------------"<<	endl;
 		cout<<"1. Insertar un nodo"<<endl;
-		cout<<"2. listar nodos usando while"<<endl;
-		cout<<"3. listar nodos usando recursividad"<<endl;
-		cout<<"4. buscar un nodo por su valor"<<endl;
-		cout<<"5. salir"<<endl<<endl;
+		//cout<<"2. listar nodos usando while"<<endl;
+		cout<<"2. listar nodos usando recursividad"<<endl;
+		cout<<"3. buscar un nodo por su valor"<<endl;
+		cout<<"4. Eliminar nodo por su valor"<<endl;
+		cout<<"5. Identificar nodo"<<endl;
+		cout<<"6. Caracteristicas de un nodo"<<endl;
+		cout<<"7. salir"<<endl<<endl;
 		cout<<"ingrese una opcion: ";
 		cin>>opcion;
 			switch(opcion){
@@ -82,36 +92,207 @@ void menu(){
 						Lista.insertarnodo(arbol,val,NULL);
 						system("pause");
 						break;
-				case 3: 
+				case 2: 
 						system("cls");
 						cout<<"--------MOSTRANDO EL ARBOL RECURSIVO--------"<<endl;
 						lista Lista2;
 						Lista2.mostrararbol(arbol);
 						system("pause");
 						break;
-				case 2: 
+						
+				case 5:
 						system("cls");
-						cout<<"--------MOSTRANDO EL ARBOL usando while(NO DISPONIBLE)--------"<<endl;
-						//lista Lista6;
-						//Lista6.mostrararbol2(arbol);
+						cout<<""<<endl;
+						cout<<"--------IDENTIFICANDO UN NODO--------"<<endl;
+						cout<<endl<<"ingrese el valor del nodo a identificar: "; cin>>val;
+						bandera=0;
+						lista lista5;
+						lista5.identificar(val,arbol);
+						if(bandera != 1){
+							cout<<endl<<"=============== NODO NO ENCONTRADO ==========================="<<endl;	
+						}			
 						system("pause");
 						break;
+						
 				case 4:
+						system("cls");
+						cout<<"-------ELIMINAR UN NODO POR SU VALOR--------------------"<<endl<<endl;
+						cout<<"ingrese el valor del nodo a eliminar: "; cin>>val;
+						bandera=0;
+						lista Lista4;
+						arbol=raiz;
+						Lista4.eliminar(val,arbol);
+						if(bandera != 1){
+							cout<<endl<<"=============== NODO NO ENCONTRADO ==========================="<<endl;	
+						}
+						system("pause");
+						break;	
+				case 6:
+						system("cls");
+						cout<<"-------CARACTERISTICAS DE UN NODO-------------"<<endl<<endl;
+						cout<<"ingrese el valor del nodo: "; cin>>val;
+						cont=0;
+						bandera=0;
+						lista lista6;
+						lista6.caracteristicas(val,arbol);
+						if(bandera != 1){
+							cout<<endl<<"=============== NODO NO ENCONTRADO ==========================="<<endl;	
+						}
+						system("pause");
+						break;			
+						
+				case 3:
 						system("cls");
 						cout<<"-------BUSAR UN NODO POR SU VALOR--------------------"<<endl<<endl;
 						int valor_buscado=0;
 						cout<<"ingrese el valor buscado: "; cin>>valor_buscado;
 						bandera=0;
 						lista Lista3;
-						Lista3.buscar(valor_buscado,arbol);
+					 	Lista3.buscar(valor_buscado,arbol);
 					 	if(bandera != 1){
 							cout<<endl<<"=============== NODO NO ENCONTRADO ==========================="<<endl;	
 						}
 						system("pause");
 						break;
+				
+				
 			}	
-	}while(opcion != 5);
+	}while(opcion != 7);
 }
+
+//funcion para describir las caracteristicas de un nodo
+
+
+//funcion para identificar un nodo
+
+
+//funcioni para eliminar un nodo y hacer que el subarbol derecho ocupe el lugar del nodo que se elimine.
+int lista::eliminar(int valor, nodo *arbol){
+	if(arbol == NULL){
+		return 0;
+	}else{
+		if(arbol->valor == valor){ //cuando lo encuentre lo va mostrar y luego procede a eliminarlo
+		anterior_eliminar=NULL;
+		//comprobamos si es un nodo hoja
+			if(arbol->izquierda == NULL && arbol->derecha == NULL){
+				anterior_eliminar=arbol->padre;
+				//ahora vemos si es hoa izq o der
+				if(anterior_eliminar->izquierda == arbol){
+					delete arbol;
+					anterior_eliminar->izquierda = NULL;
+					cout<<" ----NODO HOJA ELIMINADO----"<<endl;
+					//arbol=raiz;
+					bandera=1;
+					return 0;
+				}else if(anterior_eliminar->derecha ==arbol){
+					delete arbol;
+					anterior_eliminar->derecha = NULL;
+					cout<<" ----NODO HOJA ELIMINADO----"<<endl;
+					//arbol=raiz;
+					bandera=1;
+					return 0;
+				}
+				// en caso que no sea hoja,, cuando tenga hijo izquierdo pero no derecho
+			}else if(arbol->izquierda !=NULL && arbol->derecha == NULL){//comprobamos si tiene nodo hijio izquierdo pero no derecho
+				//aca se puede presentar dos situaciones, que el nodo a liminar sea izquierdo, o que sea hijo derecho
+				//vamos a evaludar de que situacion se trata
+				//cout<<"es cuando el nodo tiene solo hijo izquierdo, y no derecho"<<endl;
+				subizquierdo=arbol->izquierda;
+				anterior_eliminar=arbol->padre; 
+				//cout<<"el valor del padre es:  "<<anterior_eliminar->valor<<endl; system("pause"); 
+				if(anterior_eliminar->izquierda == arbol){ //si es nodo hijo izquierdo
+					//cout<<"es nodo izquierdo con hijo izquierdo"<<endl; system("pause");
+					anterior_eliminar->izquierda=subizquierdo;
+					subizquierdo->padre=anterior_eliminar;
+					delete arbol;
+					cout<<endl<<"------ nodo eliminado----------"<<endl;
+					//arbol=raiz;
+					bandera=1;
+					return 0;
+				}else if(anterior_eliminar->derecha==arbol){//si es nodo hijo derecho
+					//cout<<"es nodo izquierdo con hijo derecho"<<endl; system("pause");
+					anterior_eliminar->derecha=subizquierdo;
+					subizquierdo->padre=anterior_eliminar;
+					delete arbol; 
+					cout<<endl<<"------ nodo eliminado----------"<<endl;
+					//arbol=raiz;
+					bandera=1;
+					return 0;
+				}
+			}else if(arbol->izquierda == NULL && arbol->derecha !=NULL){
+				subderecho=arbol->derecha;
+				anterior_eliminar=arbol->padre;
+				//evaluamos si es hijo izquierdo
+				if(anterior_eliminar->izquierda==arbol){
+					anterior_eliminar->izquierda=subderecho;
+					subderecho->padre=anterior_eliminar;
+					delete arbol;
+					cout<<endl<<"------ nodo eliminado----------"<<endl;
+					//arbol=raiz;
+					bandera=1;
+					return 0;
+				}else if( anterior_eliminar->derecha == arbol){ //si es un hijo derecho
+					anterior_eliminar->derecha=subderecho;
+					subderecho->padre=anterior_eliminar;
+					delete arbol;
+					cout<<endl<<"------ nodo eliminado----------"<<endl;
+					//arbol=raiz;
+					bandera=1;
+					return 0;
+				}
+			}else if(arbol->izquierda != NULL && arbol->derecha != NULL){//si tiene hijo izquierdo y derecho
+						//cout<<" ES CUANDO TIENE DOS HIJOS"<<endl; system("pause");
+						anterior_eliminar=arbol->padre;
+						subderecho=arbol->derecha;
+						subizquierdo=arbol->izquierda;
+						
+						if(anterior_eliminar->izquierda == arbol){//es un nodo hijo izquierdo
+						//cout<<"es nodo izquierdo con dos hijos "<<endl;  system("pause");
+							anterior_eliminar->izquierda=subizquierdo;
+							subizquierdo->padre=anterior_eliminar;
+							delete arbol; 
+							cout<<endl<<"------ nodo eliminado----------"<<endl;
+							arbol=subizquierdo;
+							while(arbol!=NULL){
+								if(arbol->derecha == NULL){
+									arbol->derecha=subderecho;
+									subderecho->padre=arbol;
+									bandera=1;
+									return 0;
+								}
+								arbol=arbol->derecha;
+							}
+							
+							
+						}else if(anterior_eliminar->derecha == arbol){//es un nodo hijo derecho
+						//out<<"es nodo derecho con dos hijos "<<endl;  system("pause");
+							anterior_eliminar->derecha=subderecho;
+							subderecho->padre=anterior_eliminar;
+							delete arbol;
+							cout<<endl<<"------ nodo eliminado----------"<<endl;
+							arbol=subderecho;
+							while(arbol!=NULL){
+								if(arbol->izquierda == NULL){
+									arbol->izquierda=subizquierdo;
+									subizquierdo->padre=arbol;
+									//arbol=raiz;
+									bandera=1;
+									return 0;
+								}
+								arbol=arbol->izquierda;
+							}
+							
+						}
+					}
+		}else{
+				
+				eliminar(valor,arbol->izquierda);
+				eliminar(valor,arbol->derecha);
+			}
+	}
+}
+
+
 
 //funcion para crear nodos nuevos
 nodo *crearnodo(int n,nodo *padre){
@@ -125,6 +306,10 @@ void lista::insertarnodo(nodo *&arbol, int n, nodo *padre){
      {		//si el arbol esta vacio
             nodo *nuevo_nodo=crearnodo(n,padre);
             arbol=nuevo_nodo;
+             if (contador == 0){
+            	raiz=arbol;
+			}
+           contador++;
      }else //si el arbol tiene al menos un nodo
      {		//aca debemos preguntar a que lado del unico nodo que tiene va insertar, si a  la izq, o la derecha
   			cout<<"------------Seleccione una opcion: ---------"<<endl<<endl;
@@ -143,17 +328,16 @@ void lista::insertarnodo(nodo *&arbol, int n, nodo *padre){
 }
 
 //funcion para buscar un nodo por su valor dentro del arbol
-void lista::buscar(int v, nodo *arbol){
+int lista::buscar(int v, nodo *arbol){
 	//verificamos qu el arbol no este vacio
 	if(arbol == NULL){
-			return ;
+			return 0;
 	}else{ //si no esta vacio
 		if(arbol->valor == v){
 			cout<<"codigo_nodo: "<<arbol<<" , valor: "<<arbol->valor<<" ,nodo izq: "<<arbol->izquierda<<" ,nodo der"<<arbol->derecha<<endl;
-			encontrado=true;
-			return;
+			bandera=1;
+			return 0;
 		}else{
-				encontrado=false;
 				buscar(v,arbol->izquierda);
 				buscar(v,arbol->derecha);
 		}
@@ -175,3 +359,4 @@ void lista::mostrararbol(nodo *arbol){
 		mostrararbol(arbol->derecha);
      }
 }
+
